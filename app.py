05d262fe1d5ac4f7def359ab81a0763270b72986
neoslvh <luuvanhung171105@gmail.com>
2025-04-10 17:21:06 +0700

    first commit

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def chat():
         return jsonify({"error": f"⚠️ Lỗi không xác định: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port = port)
